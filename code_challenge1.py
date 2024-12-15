b41356d0def2a80486f7ece68302f7100793f4f6
CrisLaurencePelaez <pelaezcrislaurence@gmail.com>
2024-12-16 01:26:46 +0800

    2

--- code_challenge1.py
+++ code_challenge1.py
@@ -1,1 +1,1 @@
-print("\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t*\t*\t*\n\t\t\t\t\t\t\t\t*\t*\t*\t*\t*\n\t\t\t\t\t\t\t*\t*\t*\t*\t*\t*\t*\n\t\t\t\t\t\t\t\t*\t*\t*\t*\t*\n\t\t\t\t\t\t\t\t\t*\t*\t*\n\t\t\t\t\t\t\t\t\t\t*")
+print("\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t*\t*\t*\n\t\t\t\t\t\t\t\t*\t*\t*\t*\t     *\n\t\t\t\t\t\t\t*\t*\t*\t*\t*\t     *      *\n\t\t\t\t\t\t\t\t*\t*\t*\t*\t     *\n\t\t\t\t\t\t\t\t\t*\t*\t*\n\t\t\t\t\t\t\t\t\t\t*")
